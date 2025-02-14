45674dd1ac2b2d5a0d33c3f4276aeda9ec3f8522
Mamad78 <mamadi78.94@gmail.com>
2025-02-15 01:52:48 +0330

    cofig color to tailwincss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <h1 className="text-3xl font-bold">شروع پروژه</h1>;
+  return (
+    <h1 className="container xl:max-w-screen-lg bg-primary-100 text-primary-700 text-3xl">
+      شروع پروژه
+    </h1>
+  );
 }
 
 export default App;
