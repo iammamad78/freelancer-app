4aede09263c581e0412b5f19148c874361f82d39
Mamad78 <mamadi78.94@gmail.com>
2025-02-22 02:04:32 +0330

    customize input and btn added

--- src/features/authentication/SendOTPForm.jsx
+++ src/features/authentication/SendOTPForm.jsx
@@ -14,12 +14,10 @@ function SendOTPForm() {
             id="phonenumber"
             value={phoneNumber}
             onChange={(e) => setPhoneNumber(e.target.value)}
-            className="w-full py-3 px-4 rounded-xl text-secondary-900 border border-gray-300 hover:border-primary-500 focus:border-primary-500 focus:bg-white transition-all duration-300 ease-out focus:shadow-lg focus:shadow-primary-200"
+            className="textfield__input"
           />
         </div>
-        <button className="px-4 py-2 font-bold bg-primary-900 text-white w-full rounded-xl transition-all duration-300 hover:bg-primary-800 shadow-lg shadow-primary-300">
-          ارسال کد تایید
-        </button>
+        <button className="btn btn--primary w-full">ارسال کد تایید</button>
       </form>
     </div>
   );
