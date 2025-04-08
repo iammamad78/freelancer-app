24d943b405340be217ced844009a3942f258b863
Mamad78 <mamadi78.94@gmail.com>
2025-04-08 07:34:59 +0330

    handle sendOTP added

--- src/features/authentication/SendOTPForm.jsx
+++ src/features/authentication/SendOTPForm.jsx
@@ -1,18 +1,39 @@
 import React, { useState } from "react";
 import TextField from "../../ui/textField";
+import toast from "react-hot-toast";
+import { useMutation } from "@tanstack/react-query";
+import { getOtp } from "../../services/authService";
 
 function SendOTPForm() {
   const [phoneNumber, setPhoneNumber] = useState("");
+  const { isPending, error, data, mutateAsync } = useMutation({
+    mutationFn: getOtp,
+  });
+
+  const handleSendOtp = async (e) => {
+    e.preventDefault();
+    try {
+      const data = await mutateAsync({ phoneNumber });
+      console.log(data.message);
+      toast.success(data.message || "کد تایید با موفقیت ارسال شد");
+      setPhoneNumber("");
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "خطا در ارسال کد تایید");
+    }
+  };
+
   return (
     <div className="sm:max-w-screen-sm">
-      <form className="space-y-8">
+      <form onSubmit={handleSendOtp} className="space-y-8">
         <TextField
           label="شماره موبایل"
           name={phoneNumber}
           value={phoneNumber}
           onChange={(e) => setPhoneNumber(e.target.value)}
         />
-        <button className="btn btn--primary w-full">ارسال کد تایید</button>
+        <button type="submit" className="btn btn--primary w-full">
+          ارسال کد تایید
+        </button>
       </form>
     </div>
   );
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import SendOTPForm from "../features/authentication/SendOTPForm";
-import CheckOTPForm from "../features/authentication/CheckOTPForm";
+// import CheckOTPForm from "../features/authentication/CheckOTPForm";
 
 function Auth() {
   return (
     <div className="flex justify-center pt-10">
       <div className="w-full sm:max-w-sm">
         <SendOTPForm />
-        <CheckOTPForm />
+        {/* <CheckOTPForm /> */}
       </div>
     </div>
   );
