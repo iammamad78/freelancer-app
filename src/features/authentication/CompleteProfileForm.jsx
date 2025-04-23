7e7fd1d673eafcab4153d145db06ce6489659dbb
Mamad78 <mamadi78.94@gmail.com>
2025-04-24 02:21:39 +0330

    added handle completeProfile

--- src/features/authentication/CompleteProfileForm.jsx
+++ src/features/authentication/CompleteProfileForm.jsx
@@ -1,17 +1,38 @@
 import React, { useState } from "react";
 import TextField from "../../ui/TextField";
 import RadioInput from "../../ui/RadioInput";
+import { useMutation } from "@tanstack/react-query";
+import { completeProfile } from "../../services/authService";
+import toast from "react-hot-toast";
+import Loading from "../../ui/Loading";
 
 function CompleteProfileForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [role, setRole] = useState("");
-  console.log(role);
 
+  const { mutateAsync, isPending } = useMutation({
+    mutationFn: completeProfile,
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { user, message } = await mutateAsync({ name, email, role });
+      toast.success(message);
+      console.log(user);
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "خطا در ثبت نام");
+    }
+  };
+  
   return (
     <div className="flex justify-center pt-10">
       <div className="w-full sm:max-w-screen-sm ">
-        <form className="space-y-8 md:border md:border-secondary-300 rounded-xl p-10">
+        <form
+          onSubmit={handleSubmit}
+          className="space-y-8 md:border md:border-secondary-300 rounded-xl p-10"
+        >
           <h2 className="text-secondary-500 flex justify-center">
             اطلاعات خود را کامل کنید
           </h2>
@@ -45,7 +66,15 @@ function CompleteProfileForm() {
               checked={role === "FREELANCER"}
             />
           </div>
-          <button className="btn btn--primary w-full">تایید</button>
+          <div>
+            {isPending ? (
+              <Loading />
+            ) : (
+              <button type="submit" className="btn btn--primary w-full">
+                تایید
+              </button>
+            )}
+          </div>{" "}
         </form>
       </div>
     </div>
