3957d619d535c6b5641703f6f9000c6b7d626148
Mamad78 <mamadi78.94@gmail.com>
2025-05-01 01:50:30 +0330

    useUser added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import useUser from "../features/authentication/useUser";
 
 function Home() {
+  const { data } = useUser();
+  console.log(data);
+
   return (
     <div className="container xl:max-w-screen-xl">
       <div>صفحه اصلی</div>
