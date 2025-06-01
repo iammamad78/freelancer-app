574f9037ef807dabf715f23c7a2d244359e2ec19
Mamad78 <mamadi78.94@gmail.com>
2025-06-02 02:28:32 +0330

    useProject added

--- src/features/projects/ProjectRow.jsx
+++ src/features/projects/ProjectRow.jsx
@@ -3,18 +3,19 @@ import Table from "../../ui/Table";
 import truncateText from "../../utils/truncateText";
 import { toPersianNumbersWithComma } from "../../utils/toPersianNumbers";
 import toLocalDateShort from "../../utils/toLocalDateShort";
-import { HiOutlineTrash } from "react-icons/hi";
+import { HiEye, HiOutlineTrash } from "react-icons/hi";
 import { TbPencilMinus } from "react-icons/tb";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
 import useRemoveProject from "./useRemoveProject";
 import CreateProjectForm from "./CreateProjectForm";
 import ToggleProjectStatus from "./ToggleProjectStatus";
+import { Link } from "react-router-dom";
 
 function ProjectRow({ project, index }) {
   const [isEditOpen, setIsEditOpen] = useState(false);
   const [isDeleteOpen, setIsDeleteOpen] = useState(false);
-  const { isDeleting, removeProject } = useRemoveProject();
+  const { removeProject } = useRemoveProject();
 
   return (
     <Table.Row>
@@ -77,6 +78,11 @@ function ProjectRow({ project, index }) {
             </Modal>
           </>
         </div>
+      </td>
+      <td>
+        <Link to={project._id} className="flex justify-center">
+          <HiEye className="w-5 h-5 text-primary-800 hover:text-secondary-500" />
+        </Link>
       </td>
     </Table.Row>
   );
