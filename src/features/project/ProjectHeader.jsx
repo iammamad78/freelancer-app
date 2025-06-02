42699bd737ced4f66a0bd4876c7fe403a4254382
Mamad78 <mamadi78.94@gmail.com>
2025-06-03 02:45:07 +0330

    Proposal structure added

--- src/features/project/ProjectHeader.jsx
+++ src/features/project/ProjectHeader.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
-function ProjectHeader() {
+import React from "react";
+import { HiArrowRight } from "react-icons/hi";
+import useMoveBack from "../../hooks/useMoveBack";
+// This component renders a button with an arrow icon.
+function ProjectHeader({ project }) {
+  const moveBack = useMoveBack();
   return (
-    <div>ProjectHeader</div>
-  )
+    <div className="flex items-center gap-x-8 mb-8">
+      <button onClick={moveBack}>
+        <HiArrowRight className="w-5 h-5 text-secondary-500" />
+      </button>
+      <h1 className="font-black text-secondary-700 text-lg">
+        لیست درخواست های {project.title}
+      </h1>
+    </div>
+  );
 }
 
-export default ProjectHeader
+export default ProjectHeader;
