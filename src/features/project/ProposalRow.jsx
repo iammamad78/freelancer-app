b35dac6238a41964ae87244ef02f156e0d3ec85b
Mamad78 <mamadi78.94@gmail.com>
2025-06-04 02:26:45 +0330

    Toggle Status added

--- src/features/project/ProposalRow.jsx
+++ src/features/project/ProposalRow.jsx
@@ -2,18 +2,41 @@ import React from "react";
 import Table from "../../ui/Table";
 import truncateText from "../../utils/truncateText";
 
+const statusStyles = [
+  {
+    label: "رد شده",
+    className: "badge--danger",
+  },
+  {
+    label: "در انتظار تایید",
+    className: "badge--secondary",
+  },
+  {
+    label: "تایید شده",
+    className: "badge--success",
+  },
+];
+
 function ProposalRow({ proposal, index }) {
+  const { status, user } = proposal;
+
   return (
     <Table.Row>
       <td>{index + 1}</td>
-      <td>{proposal.user.name}</td>
+      <td>{user.name}</td>
       <td>
         <p>{truncateText(proposal.description, 50)}</p>
       </td>
       <td>{proposal.duration} روز</td>
       <td>{proposal.price}</td>
-      <td>{proposal.status}</td>
-      <td>++</td>
+      <td>
+        <span className={`badge ${statusStyles[status].className}`}>
+          {statusStyles[status].label}
+        </span>
+      </td>
+      <td>
+        
+      </td>
     </Table.Row>
   );
 }
