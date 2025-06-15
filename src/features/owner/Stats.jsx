4e51019424c6d5e5a9e3f205f7d7c8812fbfab28
Mamad78 <mamadi78.94@gmail.com>
2025-06-15 03:49:55 +0330

    Stat added

--- src/features/owner/Stats.jsx
+++ src/features/owner/Stats.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import Stat from "./Stat";
+import {
+  HiCollection,
+  HiCurrencyDollar,
+  HiOutlineViewGrid,
+} from "react-icons/hi";
 
 function Stats({ projects }) {
   const numOfProjects = projects.length;
@@ -12,7 +18,28 @@ function Stats({ projects }) {
     0
   );
 
-  return <div>Stats</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <Stat
+        color="primary"
+        title="پروژه ها"
+        value={numOfProjects}
+        icon={<HiOutlineViewGrid className="w-20 h-20" />}
+      />
+      <Stat
+        color="green"
+        title="پروژه های واگذار شده"
+        value={numOfAcceptedProjects}
+        icon={<HiCurrencyDollar className="w-20 h-20" />}
+      />
+      <Stat
+        color="blue"
+        title="درخواست ها"
+        value={numOfProposals}
+        icon={<HiCollection className="w-20 h-20" />}
+      />
+    </div>
+  );
 }
 
 export default Stats;
