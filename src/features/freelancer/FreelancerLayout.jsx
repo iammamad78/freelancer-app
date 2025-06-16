35350ab9fd69cd63bf5b013fe231edb66edbf675
Mamad78 <mamadi78.94@gmail.com>
2025-06-16 03:33:01 +0330

    FreelancerLayout added

---
+++ src/features/freelancer/FreelancerLayout.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import AppLayout from "../../ui/AppLayout";
+import Sidebar from "../../ui/Sidebar";
+import CustomNavLink from "../../ui/CustomNavLink";
+import { HiCollection, HiHome } from "react-icons/hi";
+import { MdEditDocument } from "react-icons/md";
+
+function FreelancerLayout() {
+  return (
+    <AppLayout>
+      <Sidebar>
+        <CustomNavLink to="dashboard">
+          <HiHome />
+          <span>داشبورد</span>
+        </CustomNavLink>
+
+        <CustomNavLink to="projects">
+          <HiCollection />
+          <span>پروژه ها</span>
+        </CustomNavLink>
+
+        <CustomNavLink to="proposals">
+          <MdEditDocument />
+          <span>درخواست ها</span>
+        </CustomNavLink>
+      </Sidebar>
+    </AppLayout>
+  );
+}
+
+export default FreelancerLayout;
