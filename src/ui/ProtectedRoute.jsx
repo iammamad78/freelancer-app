55b98433f00aae1cee5b6bf275c149bb95bee431
Mamad78 <mamadi78.94@gmail.com>
2025-07-06 16:35:06 +0330

    ProtectedRoute added

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useAuthorized from "../features/authentication/useAuthorized";
+import { useNavigate } from "react-router-dom";
+import Loading from "../ui/Loading";
 
 function ProtectedRoute({ children }) {
-  const { user, isLoading, isAuthenticated, isAuthorized } = useAuthorized();
+  const navigate = useNavigate();
 
-  return children;
+  const { isLoading, isAuthenticated, isAuthorized } = useAuthorized();
+
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) navigate("/auth");
+    if (!isAuthorized && isLoading) navigate("/not-access", { replace: true });
+  }, [isLoading, isAuthenticated, isAuthorized, navigate]);
+
+  if (isLoading)
+    return (
+      <div className="flex items-center justify-center h-screen bg-secondary-100">
+        <Loading />
+      </div>
+    );
+
+  if (isAuthenticated && isAuthorized) return children;
 }
 
 export default ProtectedRoute;
